c5512c89cb3f101cc864c9fe9732974553a18136
yotaadachi <yota961219@gmail.com>
2022-04-23 22:24:14 +0900

    020_変数の定義と型

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,14 @@
+// var, let, const
+
+let name = 'Tom';
+name = 'Tim';
+console.log('hello ' + name);
+
+// Number, String, Boolean, Undefined, Null, Symbol
+
+// 動的型付け言語
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+console.log(typeof variable);
