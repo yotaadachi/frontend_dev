cb7564a16762785e60f0f4645d6a95da9f6ee777
yotaadachi <yota961219@gmail.com>
2022-04-29 16:40:16 +0900

    070_関数定義とアロー関数

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,12 @@
+// function hello(name = 'Tom') {
+//   console.log('hello ' + name);
+// }
+
+const hello = (name, age) => console.log('hello ' + name + ' ' + age);
+
+hello('Code Mafia', 10);
+hello('Code Mafia2', 20);
+
+const arry = [1,2,3,4,5,6];
+
+arry.forEach(value => console.log(value));
